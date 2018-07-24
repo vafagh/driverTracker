a32d5893ca71b9dcfa43ba426a0687d1e5e6f7a0
Vafa Ghoreyshi <goreyshi@gmail.com>
2018-07-24 17:52:26 -0500

    New action permition and route

--- resources/views/layouts/action.blade.php
+++ resources/views/layouts/action.blade.php
@@ -1,3 +1,33 @@
+@php
+    $user_role = Auth::user() && Auth::user()->role_id;
+    $title = 'Done';
+    if ($rideable->type=='Delivery')   $title = 'Delivered';
+    if($rideable->type=='Pickup')  $title = 'Picked up';
+    // if($ridable->location->name='CertiFit')  $title = 'Dropped off';
+@endphp
+@switch($action)
+    @case('Created')
+        @if ($user_role = 3 )<a class="badge badge-success" href="/ride/create/{{$rideable->id}}/">Assign</a>@endif
+    @break
+
+    @case('On The Way')
+        @if ($user_role = 2 || $user_role = 3 )<a class="badge badge-warning" href="/rideable/{{$rideable->id}}/NON.A">NON/A</a>@endif
+        @if ($user_role = 3 || $user_role = 4 )<a class="badge badge-primary" href="/rideable/{{$rideable->id}}/Done">{{$title}}</a>@endif
+    @break
+
+    @case('Reactived')
+        @if ($user_role = 2 || $user_role = 3 )<a class="badge badge-warning" href="/rideable/{{$rideable->id}}/NON.A">NON/A</a>@endif
+        @if ($user_role = 3 || $user_role = 4 )<a class="badge badge-primary" href="/rideable/{{$rideable->id}}/Done">{{$title}}</a>@endif
+    @break
+
+    @case('NON.A')
+        @if ($user_role = 2 )<a class="badge badge-info" href="/rideable/{{$rideable->id}}/Reactived">Re-active</a>@endif
+        @if ($user_role = 2 || $user_role = 3 )<a class="badge badge-danger" href="/rideable/{{$rideable->id}}/Canceled">Cancel</a>@endif
+    @break
+
+    @default
+@endswitch
+{{--
 @switch($action)
     @case('On The Way')
         <a class="badge badge-warning" href="/rideable/{{$rideable->id}}/NON.A">NON/A</a>
@@ -25,13 +55,15 @@
         @if (Auth::user() && Auth::user()->role_id >= 3 )
             <a class="badge badge-success" href="/ride/create/{{$rideable->id}}/">Assign</a>
         @endif
-    <a class="badge badge-dark" href="/rideable/{{$rideable->id}}/Archived">Archive</a>
-    <a class="badge badge-info" href="/rideable/{{$rideable->id}}/WaitingForJoe">Activate</a>
-    <a class="badge badge-success" href="/rideable/{{$rideable->id}}/WaitingForDropOff">Drop off</a>
+        @if (Auth::user() && Auth::user()->role_id >= 4 )
+            <a class="badge badge-dark" href="/rideable/{{$rideable->id}}/Archived">Archive</a>
+        @endif
+        <a class="badge badge-info" href="/rideable/{{$rideable->id}}/Reactived">Re-active</a>
+        <a class="badge badge-success" href="/rideable/{{$rideable->id}}/WaitingForDropOff">Drop off</a>
     @break
 
     @case('Archived')
-    Trip Done
+        Trip Done
     @break
 
     @default
@@ -41,4 +73,4 @@
         <a class="badge badge-warning" href="/rideable/{{$rideable->id}}/NON.A">NON/A</a>
         <a class="badge badge-danger" href="/rideable/{{$rideable->id}}/Canceled">Cancel</a>
         <a class="badge badge-success" href="/rideable/{{$rideable->id}}/WaitingForDropOff">Drop off</a>
-@endswitch
+@endswitch --}}
