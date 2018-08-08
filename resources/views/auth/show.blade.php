5977cf7a1b857f499476d458974660d187ecd83e
Vafa Ghoreyshi <goreyshi@gmail.com>
2018-08-08 03:25:39 -0500

    Styling the logs

--- resources/views/auth/show.blade.php
+++ resources/views/auth/show.blade.php
@@ -17,24 +17,22 @@
                         'file'=>true])
                     @endcomponent
                 </div>
-
             </div>
             <div class="card-body">
                 <ul class="list-group" id='userd'>
                     <li class="row m-0 p-0 mb-1">
                         {{-- <img class="w-100" src="{{($user->image == null) ? '/img/def.svg' : '/img/user/'.$user->image }}" alt=""> --}}
-
                         <div class="col-4">
                             <div>Name: {{$user->name}}</div>
                             <div>Email: {{$user->email}}</div>
                             <div>Role: {{$user->role_id}}</div>
                         </div>
-
                         <div class="col-4">
                             Created at: {{$user->created_at}}<br>
                             Updated at: {{$user->updated_at}}
                         </div>
-                        <div class="col-4 text-right pt-2">@if (Auth::user()->role_id > 3)
+                        <div class="col-4 text-right pt-2">
+                            @if (Auth::user()->role_id > 3)
                                 @component('layouts.components.modal',[
                                     'modelName'=>'user',
                                     'action'=>'edit',
@@ -50,76 +48,49 @@
                     </li>
                 </ul>
             </div>
-            <div class="card">
-                <div class="card-header row m-0">
-                    All Transaction
+            <h5 class="mx-4">Activity:</h5>
+            <div class="card-body">
+
+                <div class="pagination">
+                    {{ $transactions->links("pagination::bootstrap-4") }}
                 </div>
-                <div class="card-body">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <td colspan="6">
-                                    <div class="pagination">
-                                        {{ $transactions->links("pagination::bootstrap-4") }}
+                <div class="header row">
+                    <div class="col-4 col-sm-4 col-md-3 col-lg-2">Table</div>
+                    <div class="col-2 col-sm-2 col-md-3 col-lg-2">Row</div>
+                    <div class="col-3 col-sm-3 col-md-3 col-lg-6">Action</div>
+                    <div class="col-3 col-sm-3 col-md-3 col-lg-2">Create</div>
+                </div>
+
+                <div id="accordion">
+                    @foreach ($user->transactions->sortByDesc('id') as $key => $transaction)
+                        <div class="card mb-1">
+                            <div class="card-header" id="heading{{$key}}">
+                                    <div class="h5 my-0 row" data-toggle="collapse" data-target="#collapse{{$key}}" aria-expanded="true" aria-controls="collapse{{$key}}">
+                                        <a    class="col-6 col-sm-4 col-md-3 col-lg-2" href="/{{$transaction->table_name}}">{{$transaction->table_name}}</a>
+                                        <a    class="col-2 col-sm-2 col-md-3 col-lg-2" href="/{{str_singular($transaction->table_name)}}/show/{{$transaction->row_id}}">{{$transaction->row_id}}</a>
+                                        <span class="col-6 col-sm-3 col-md-3 col-lg-6 {{($transaction->action=='destroy') ? 'text-danger':''}}">{{$transaction->action}}</span>
+                                        <span class="col-10 col-sm-3 col-md-3 col-lg-2" title="{{$transaction->created_at}}">{{$transaction->created_at->diffForHumans()}}</span>
                                     </div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <th>Create</th>
-                                <th>Action</th>
-                                <th>Table</th>
-                                <th>Row</th>
-                                <th>Details</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($user->transactions->sortByDesc('id') as $key => $transaction)
-                                <tr>
-
-                                    <td>
-                                        <span title="{{$transaction->created_at}}">{{$transaction->created_at->diffForHumans()}}</span>
-                                    </td>
-
-                                    <td>
-                                        {{$transaction->action}}
-                                    </td>
-
-                                    <td>
-                                        {{$transaction->table_name}}
-                                    </td>
-
-                                    <td>
-                                        {{$transaction->row_id}}
-                                    </td>
-
-                                    <td class="fixesWidthFont">
-                                        <div class="text-danger">
-                                            {{-- <strong>Old: </strong> --}}
-                                            {{-- {{$transaction->last_data}} --}}
-                                            @component('layouts.row',['data' =>$transaction->last_data])
-                                            @endcomponent
-                                        </div>
-                                        <div class="text-success">
-                                            <strong>New: </strong>
-                                            {{$transaction->new_data}}
-                                        </div>
-                                    </td>
-
-
-                                </tr>
-                            @endforeach
-                            <tr>
-                                <td colspan="6">
-                                    <div class="pagination">
-                                        {{ $transactions->links("pagination::bootstrap-4") }}
+                            </div>
+                            <div id="collapse{{$key}}" class="collapse" aria-labelledby="heading{{$key}}" data-parent="#accordion">
+                                <div class="card-body">
+                                    <div class="text-danger">
+                                        @component('layouts.row',['data' =>$transaction->last_data])
+                                        @endcomponent
                                     </div>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                                    <div class="text-success">
+                                        @component('layouts.row',['data' =>$transaction->new_data])
+                                        @endcomponent
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <div class="pagination">
+                    {{ $transactions->links("pagination::bootstrap-4") }}
                 </div>
             </div>
-
         </div>
     </div>
 @endsection
--- resources/views/layouts/row.blade.php
+++ resources/views/layouts/row.blade.php
@@ -1,12 +1,14 @@
-<div class="">
-    {{$data}}
-    {{-- @php
-        $ardata = json_decode($data, true);
-        // dd(($ardata));
+<div class="row">
+    @php
+    $ardata = json_decode($data, true);
+    // dd($ardata);
     @endphp
-    @foreach ($ardata as $key => $value)
-    {{implode( $value)}}
-        {{-- <div>{{$key}}</div>
-        <div>{{$value}}</div> 
-    @endforeach --}}
+    @if (gettype($ardata)=='array')
+        @foreach ($ardata as $key => $value)
+            <div class="border mr-1 px-1 mb-1">
+                <div class="border-bottom ">{{$key}}</div>
+                <div>{{$value}}</div>
+            </div>
+        @endforeach
+    @endif
 </div>
