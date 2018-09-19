dcb9bab8144173d43229bfd88b582676c4532694
Vafa Ghoreyshi <goreyshi@gmail.com>
2018-09-19 11:09:43 -0500

    disable user self register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,9 +61,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            <li class="nav-item">
+                            {{-- <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
+                            </li> --}}
                         @else
                             <li class="nav-item">
                                 <form method="get" action="/search/">
