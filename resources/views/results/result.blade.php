67e5aa4afce1f57b9f83e51f185f5fd0d1049806
Vafa Ghoreyshi <goreyshi@gmail.com>
2018-09-19 16:00:58 -0500

    Date picker form for search

--- resources/views/results/result.blade.php
+++ resources/views/results/result.blade.php
@@ -6,21 +6,38 @@
         </div>
         @if ($invoices->count()+$drivers->count()+$trucks->count()+$locations->count()+$fillups->count()>0)
 
-            @component('results.show',['colection'=>$invoices])@endcomponent
+            @component('results.show',['colection'=>$invoices])
+            @endcomponent
 
-            @component('results.show',['colection'=>$drivers])@endcomponent
+            @component('results.show',['colection'=>$drivers])
+            @endcomponent
 
-            @component('results.show',['colection'=>$trucks])@endcomponent
+            @component('results.show',['colection'=>$trucks])
+            @endcomponent
 
-            @component('results.show',['colection'=>$locations])@endcomponent
+            @component('results.show',['colection'=>$locations])
+            @endcomponent
 
-            @component('results.show',['colection'=>$fillups])@endcomponent
+            @component('results.show',['colection'=>$fillups])
+            @endcomponent
 
         @else
             <div class="card-header">
                 No match found!
             </div>
         @endif
+        <form method="get" action="/search/">
+            <div class="row pb-4 m-auto">
 
+                <div class="form-control border border-white col-12 col-md-7 col-lg-8 text-right">
+                    Search by created date
+                </div>
+                <div class="col-12 col-md-5 col-lg-4 row">
+                    <input name="q" class="form-control d-inline w-75" type="date">
+                    <button class="form-control btn d-inline w-25" type="submit">Search</button>
+                </div>
+            </div>
+            {{ csrf_field() }}
+        </form>
     </div>
 @endsection
