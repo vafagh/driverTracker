9ad9fc768dfefdf446428a7e68c49711eb91be67
Vafa Ghoreyshi <goreyshi@gmail.com>
2019-07-24 12:52:22 -0500

    UI fix

--- resources/views/rideable/locationAssign.blade.php
+++ resources/views/rideable/locationAssign.blade.php
@@ -14,25 +14,30 @@
 <div class="card-header  m-0 justify-around">
     @if ($unassignLocations && $driver->truck_id != null)
 
-        <i class="material-icons md-1">store_mall_directory</i>
-        <span class="btn btn-sm ml-4">Un-assigned deliveries:</span>
+
+        <span class="btn btn-sm ml-1">
+            <i class="material-icons">store_mall_directory</i>
+            Shops with active ticket:
+        </span>
 
         @foreach ($unassignLocations as $location)
-            <div class="add d-inline btn btn-secondary mr-1">
-                {{$location->name}}
-                <a class="badge badge-info{{(Auth::user()->role_id>3 && $location->type=='DropOff') ?' d-none':''}} badge-sm  h-75" title="{{$location->name}}" href="/location/{{$location->id}}/{{$driver->id}}/{{$today}}/Morning">
-                    <i class="material-icons md-14 text-dark">add</i>
+            <div class="d-inline btn btn-sm btn-secondary mr-1 my-1">
+                {{-- <a class="text-white" title="{{$location->long}}" href="/location/show/{{$location->id}}" target="_blank"> --}}
+                    {{$location->name}}
+                {{-- </a> --}}
+                <a class="bg-info{{(Auth::user()->role_id>3 && $location->type=='DropOff') ?' d-none':''}}  " title="{{$location->name}}" href="/location/{{$location->id}}/{{$driver->id}}/{{$today}}/Morning">
+                    <i class="material-icons text-dark">add</i>
                 </a>
-                <a class="badge badge-warning{{(Auth::user()->role_id>3 && $location->type=='DropOff') ?' d-none':''}} badge-sm text-white h-75" title="{{$location->name}}" href="/location/{{$location->id}}/{{$driver->id}}/{{$today}}/Evening">
-                    <i class="material-icons md-14 text-dark">add</i>
+                <a class="bg-warning{{(Auth::user()->role_id>3 && $location->type=='DropOff') ?' d-none':''}}  ml-1" title="{{$location->name}}" href="/location/{{$location->id}}/{{$driver->id}}/{{$today}}/Evening">
+                    <i class="material-icons text-dark">add</i>
                 </a>
             </div>
         @endforeach
 
         @if (empty($date))
-            <div class="add d-inline"><a class="btn btn-danger{{(Auth::user()->role_id==3) ?' d-none':''}} btn-sm text-white h-75" title="See other days" href="?date=all"><i class="material-icons md-14 text-dark">calendar_today</i>More</a></div>
+            <div class="add d-inline"><a class="{{(Auth::user()->role_id==3) ?' d-none':''}} " title="See other days" href="?date=all"><i class="material-icons md-14 text-dark">calendar_today</i>More</a></div>
         @else
-            <div class="add d-inline"><a class="btn btn-danger{{(Auth::user()->role_id==3) ?' d-none':''}} btn-sm text-white h-75" title="Only Today's" href="?date="><i class="material-icons md-14 text-dark">today</i>Less</a></div>
+            <div class="add d-inline"><a class="{{(Auth::user()->role_id==3) ?' d-none':''}} " title="Only Today's" href="?date="><i class="material-icons md-14 text-dark">today</i>Less</a></div>
         @endif
     @else
         <div class="">
