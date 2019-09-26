3b5a641901aa5cd99cb4824cf587c36aaea0060e
Vafa Ghoreyshi <goreyshi@gmail.com>
2019-09-26 17:59:22 -0500

    Add a hover effect, code clean up

--- resources/views/service/services.blade.php
+++ resources/views/service/services.blade.php
@@ -17,20 +17,13 @@
             </div>
         </div>
         <div class="card-body">
-            {{-- <div class='row font-weight-light h6'>
-            <div class="{{$tr='col-4 col-sm-4 col-md-3 col-lg-3 col-xl-2'}}">Truck<br>Driver</div>
-            <div class="{{$ser='col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2'}}">Service/<br>Description</div>
-            <div class="{{$shop='col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2'}}">Shop/ <br>Mileage</div>
-            <div class="{{$tot='col-4 col-sm-3 col-md-1 col-lg-1 col-xl-2'}} text-truncate"><strong>Total</strong><br>Voucher</div>
-            <div class="{{$on='col-4 col-sm-4 col-md-2 col-lg-2 col-xl-2'}}">On</div>
-            <div class="{{$ac='col-4 col-sm-5 col-md-2 col-lg-2 col-xl-2'}}">Actions</div>
-        </div> --}}
+
         <div id="accordion">
             @foreach ($trucks as $key => $truck)
                 @if ($truck->status==1)
                     <div class="card mb-1">
-                        <div class="card-header font-weight-light h6 bg-white" id="{{$truck->id}}">
-                            <a class="btn btn-link d-block text-left" data-toggle="collapse" data-target="#col-{{$truck->id}}" aria-expanded="false" aria-controls="col-{{$truck->id}}">
+                        <div class="card-header font-weight-light h6 bg-white " id="{{$truck->id}}">
+                            <a class="btn btn-link d-block text-left list-group-item-action" data-toggle="collapse" data-target="#col-{{$truck->id}}" aria-expanded="false" aria-controls="col-{{$truck->id}}">
                                 @php
                                     $driver = App\Driver::where('truck_id',$truck->id)->first();
                                 @endphp
